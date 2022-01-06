749de037e38ff4478433aa8f0d141968361c9865
AZorana <zaleksic656@gmail.com>
2022-01-06 22:49:52 +0100

    Modified code for robust and non-robust Mahalanobis distance for continuous data

--- Robust and Non-robust Mahalanobis distance for continuous data.R
+++ Robust and Non-robust Mahalanobis distance for continuous data.R
@@ -1,6 +1,7 @@
 library(robustbase)
 library(dplyr)
 library(ggplot2)
+library(robust)
 
 data = airquality[c("Ozone" , "Temp")]
 data
@@ -20,7 +21,7 @@ d1 = d %>% arrange(desc(MD))
 (MDrobust_center = MDrobust$center)
 (MDrobust_cov = MDrobust$cov)
 
-
+r = fastmcd(data)
 
 rad  = sqrt(qchisq(p = 0.95 , df = ncol(data)))
 
@@ -36,12 +37,15 @@ figure = ggplot(data , aes(x = Ozone , y = Temp)) +
   geom_polygon(data = ellipse , fill = "lightgreen" , color = "green" , alpha = 0.5)+
   geom_point(aes(center[1] , center[2]) , size = 5 , color = "brown") +
   geom_text( aes(label = row.names(data)) , hjust = 1 , vjust = -1.5 ,size = 2.5 ) +
-  ylab("Температура") + xlab("Озон") +
+  ylab("Temperatura") + xlab("Ozon") +
   ggtitle(title) + theme(plot.title = element_text(hjust = 0.5))
 return(figure)
 }
 
-Ellipse_plot(data,MD_center, cov(data), rad,"Неробусно Махаланобисово растојање")
-Ellipse_plot(data,MDrobust_center, MDrobust_cov, rad, "Робусно Махаланобисово растојање")
+Ellipse_plot(data,MD_center, cov(data), rad,"Nerobusno Mahalanobisovo rastojanje")
+Ellipse_plot(data,MDrobust_center, MDrobust_cov, rad, "Robusno Mahalanobisovo rastojanje")
 
+# Grafik zavisnosti robusnog i nerobusnog  Mahalanobisovog rastojanja
 
+plot(mahalanobis(data, colMeans(data), cov(data)), MDrobust$mah,  
+     ylab = "Robusno Mahalanobisovo rastojanje", xlab = "Mahalanobisovo rastojanje", col="dark red" )
