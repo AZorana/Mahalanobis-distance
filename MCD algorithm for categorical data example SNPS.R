9c908f018a2f6692f70b5f9fa5c31e93d07d075c
AZorana <zaleksic656@gmail.com>
2022-01-06 22:51:53 +0100

    Modified code for MCD algorithm for categorical data example SNPS

--- MCD algorithm for categorical data example SNPS.R
+++ MCD algorithm for categorical data example SNPS.R
@@ -1,8 +1,9 @@
 library(GSVD)
 library(ours)
+library(klaR)
 set.seed(12345)
 # Baza se nalazi na adresi https://github.com/derekbeaton/OuRS/tree/master/OuRS/data
-snps=SNPS
+snps = SNPS
 
 
 outliers_plot_generalizedMD = function(data, alpha){
@@ -15,8 +16,32 @@ outliers_plot_generalizedMD = function(data, alpha){
   return(cbind(sort(cat_mcd$dists$mahal_dists), sort( cat_mcd$dists$robust_mahal_dists)))
 } 
 
-(r_0.5 = outliers_plot_generalizedMD(snps, 0.75))
+(r_0.75 = outliers_plot_generalizedMD(snps, 0.75))
 
-snps_without_outliers=snps[c(-23,-53), ]
+snps_without_outliers = snps[c(-23,-53), ]
 
-(r_0.5_without_outliers = outliers_plot_generalizedMD(snps_without_outliers, 0.75))
+(r_0.75_without_outliers = outliers_plot_generalizedMD(snps_without_outliers, 0.75))
+
+
+
+optimalan_broj_K = function(data){
+  sum_withindiff = c()
+  broj_K = 0
+  for (i in 2:6) {
+    cl = kmodes(data, modes = i)
+    sum_withindiff[i-1] = sum(cl$withindiff)
+  }
+  return(sum_withindiff)
+}
+(b = optimalan_broj_K(snps))
+
+plot(c(2,3,4,5,6), b, xlab = "Broj klastera" , ylab = "Suma withindiff" ,type = "l", col = "blue" )
+
+
+(cl = kmodes(snps, modes = 4))
+(sum_withindiff=sum(cl$withindiff))
+
+
+(cl_bez_autlajera = kmodes(snps_without_outliers,4))
+(sum_withindiff1=sum(cl_bez_autlajera$withindiff))
+
